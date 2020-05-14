b02dcc5daab100834b444653c4059f53e5554d80
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-05-15 00:57:52 +0200

    Initial upload

---
+++ DiscordGit/CommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System;
+
+namespace DiscordGit
+{
+	public class CommandHandler
+	{
+		private readonly string repoLink = Environment.GetEnvironmentVariable("DiscordBotRepoLink");
+		private readonly DiscordSocketClient client;
+		private readonly CommandService commands;
+
+		public CommandHandler(DiscordSocketClient client, CommandService commands)
+		{
+			this.commands = commands;
+			this.client = client;
+		}
+
+		public async Task InstallCommandsAsync()
+		{
+			client.MessageReceived += ReferenceIssuesAsync;
+		}
+
+		private async Task ReferenceIssuesAsync(SocketMessage messageArgs)
+		{
+			var message = messageArgs as SocketUserMessage;
+			var context = new SocketCommandContext(client, message);
+
+			if (message == null) return;
+			var pattern = "#[0-9]*";
+			var x = Regex.Matches(message.Content, pattern);
+			foreach(Match match in Regex.Matches(message.Content, pattern))
+				await context.Channel.SendMessageAsync($"https://github.com/godotengine/godot/issues/{match.Value.Substring(1)}");
+		}
+	}
+}
--- DiscordGit/Program.cs
+++ DiscordGit/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace DiscordGit
 {
 	class Program
 	{
+		private DiscordSocketClient client;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			new Program().MainAsync().GetAwaiter().GetResult();
+		}
+
+		public async Task MainAsync()
+		{
+			client = new DiscordSocketClient();
+			var cmdHandler = new CommandHandler(client, new CommandService());
+
+			client.Log += Log;
+
+			await cmdHandler.InstallCommandsAsync();
+			await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+			await client.StartAsync();
+
+			await Task.Delay(-1);
+		}
+
+		private Task Log(LogMessage arg)
+		{
+			Console.WriteLine($"{arg.Message}");
+			return Task.CompletedTask;
 		}
 	}
 }
